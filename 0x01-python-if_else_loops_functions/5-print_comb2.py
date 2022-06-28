61d15fbecece3f39f80ae320c1677678241015e0
Brook Endale <bekelebrook24@gmail.com>
2022-06-28 16:46:54 +0300

    python script that prints integers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 for number in range(100):
     if (number != 99)
         print("{}{}, ".format(int(number / 10), number % 10), end="")
     else:
-        print("{}{}".fomrat(int(number / 10), number % 10))
+        print("{}{}".format(int(number / 10), number % 10))
