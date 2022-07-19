32469e854561d68fcae86774f685c2d69bf59a5e
Brook Endale <bekelebrook24@gmail.com>
2022-07-19 20:50:20 +0300

    check edits

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    check_div = []
-
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            check_div.append(True)
+    new_list = []
+    
+    for i in my_list:
+        if i % 2 == 0:
+            new_list += [True]
         else:
-            check_div.append(False)
-
-    return (check_div)
+            new_list += [False]
+    return new_list
