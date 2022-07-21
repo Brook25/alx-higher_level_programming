6b28b7ba0c523c155a5e9825f75b669bf2f60b87
Brook Endale <bekelebrook24@gmail.com>
2022-07-21 12:50:41 +0300

    printing roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,12 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
     rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    sum = 0
+    add = 0
     stg = roman_string
     for i in range(len(stg)):
         for key in rom_dict:
             if stg[i] == key:
-                sum += rom_dict[key]
+                add += rom_dict[key]
                     if i < len(stg) - 1 and rom_dict[stg[i]] < rom_dict[stg[i + 1]]:
-                        sum -= 2 * rom_dict[stg[i]]
-    return sum
+                        add -= 2 * rom_dict[stg[i]]
+    return add
