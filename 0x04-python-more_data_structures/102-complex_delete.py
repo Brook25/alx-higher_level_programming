03ba7bb9c204c2499b205089f9867c2b3dbdf7de
Brook Endale <bekelebrook24@gmail.com>
2022-07-21 18:57:20 +0300

    function deletes a key based on a value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    b_dictionary = a_dictionary.copy()
-    for key, val in b_dictionary.items():
+    for key, val in b_dictionary.copy().items():
         if value == val:
             del a_dictionary[key]
-    b_dictionary.clear()
     return a_dictionary
