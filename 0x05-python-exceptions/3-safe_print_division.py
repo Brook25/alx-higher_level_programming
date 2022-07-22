694ffdaa759d9fd6597b40947973c5141fc52138
Brook Endale <bekelebrook24@gmail.com>
2022-07-22 20:12:25 +0300

    function safe prints division of integers and handles exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div = a / b
-    except (ZeroDivisionError, FloatingPointError):
-        div = None
+        val = a / b
+        return a / b
+    except ZeroDivisionError:
+        val = None
+        return val
     finally:
-        print("Inside result: {}".format(div))
-    return div
+        print("Inside result: {}".format(val))
