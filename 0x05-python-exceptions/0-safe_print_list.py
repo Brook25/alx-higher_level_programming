de885963f8db8650bf133a1241c17d2f388ef2aa
Brook Endale <bekelebrook24@gmail.com>
2022-07-22 16:11:06 +0300

    function safe prints with try and except statements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    val = 0
-    for i in range(x):
+    j = 0
+    for elem in my_list:
         try:
-            print("{}".format(my_list[i]), end="")
-            val += 1
+            j += 1
+            if x >= j:
+                print(elem, end="")
         except IndexError:
-            break
-    print("")
-    return (val)
+            print("Error")
+    print()
+    return j
