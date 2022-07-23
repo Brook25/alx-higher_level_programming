48463a376ff501125fc5a2a552c69e26e1afc229
Brook Endale <bekelebrook24@gmail.com>
2022-07-23 11:30:47 +0300

    function safe prints integers and prints error to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,9 +2,8 @@
 def safe_print_integer_err(value):
     import sys
     try:
-        print("{:d}".format(value), end="")
-        print()
+        print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except (ValueError, TypeError) as err:
         sys.stderr.write("Exception: {}\n".format(err))
     return False
