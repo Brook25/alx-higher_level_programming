4a6d8e35cd71d09cec06c67a9d657b25bb70160b
Brook Endale <bekelebrook24@gmail.com>
2022-08-02 20:25:43 +0300

    Function creates a node class and a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,77 +1,73 @@
 #!/usr/bin/python3
-"""This module contains a class which defines a square.
-In the Square class we initialize each object by the
-__init__ method with a private instance variable called
-__size which takes the size variable's value passed as
-argument. Also checks if the size arg has a valid value.
+"""
+This Module defines classes Node and Singly linked list
+In the Node Class a Node of a sinlgy linked list is initialized.
+In the Class sinlgy linked list a singly linked list is initialized.
+and also a method is provided where the list can be sorted while insertion
+of an element
 """
 
 
-class Node():
-    """Node Class."""
+class Node:
+    """Class Node"""
 
-    def __init__(self, data, next_node=None):
-        """Initialization of Node Class"""
+    def __init__(self, data=0, next_node=None):
+        """Initialize a Node object"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Data"""
+        """Property checker for Data to be stored inside a Node object"""
         return self.__data
 
     @data.setter
-    def data(self, DataValue):
-        """Set data"""
-        if type(DataValue) != int:
+    def data(self, value):
+        """Data setter"""
+        if type(value) is not int:
             raise TypeError("data must be an integer")
-        self.__data = DataValue
+        else:
+            self.__data = value
 
     @property
     def next_node(self):
-        """Node"""
+        """property checker for next_node attribute"""
         return self.__next_node
 
     @next_node.setter
-    def next_node(self, NodeValue):
-        """set Node"""
-        if NodeValue is not None and not isinstance(NodeValue, Node):
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = NodeValue
+    def next_node(self, value):
+        """next_node setter"""
+        if isinstance(value, Node) is not True and value is not None:
+            raise TypeError("next_node must be a node object")
+        else:
+            self.__next_node = value
 
 
-class SinglyLinkedList():
-    """Class SinglyLinkedList"""
+class SinglyLinkedList:
+    """Class Singly linked list"""
     def __init__(self):
-        """Initialization of SinglyLinkedList"""
-        self.__head = None
+        """Initialization of singly linked list"""
+        self.__head = Node()
 
-    def sorted_insert(self, DataValue):
-        """Inserts a nodes"""
-        NewNode = Node(DataValue)
-        if self.__head is None:
-            self.__head = NewNode
-            return
-        if DataValue < self.__head.data:
-            NewNode.next_node = self.__head
-            self.__head = NewNode
-            return
-        actual = self.__head
-        while DataValue >= actual.data:
-            prev = actual
-            if actual.next_node:
-                actual = actual.next_node
-            else:
-                actual.next_node = NewNode
-                return
-        prev.next_node = NewNode
-        NewNode.next_node = actual
+    def sorted_insert(self, value):
+        """Object Method to implement sorted insert"""
+        if self.__head.next_node is None:
+            self.__head.next_node = Node(value, None)
+        else:
+            temp = self.__head.next_node
+            temp1 = self.__head
+            while temp is not None and temp.data < value:
+                temp1 = temp
+                temp = temp.next_node
 
-    def __str__(self):
-        """Class As a String"""
-        strg = ""
-        actual = self.__head
-        while actual:
-            strg += str(actual.data) + "\n"
-            actual = actual.next_node
-        return strg[:-1]
+            new_node = Node(value, temp)
+            temp1.next_node = new_node
+
+    def __repr__(self):
+        """Makes the List printable"""
+        out_put = ""
+        temp2 = self.__head.next_node
+        while temp2 is not None:
+            out_put += str(temp2.data) + '\n'
+            temp2 = temp2.next_node
+        return out_put[:-1]
