a0d3e54ca0851e24745892788d9c9799a699eebd
Brook Endale <bekelebrook24@gmail.com>
2022-08-07 11:56:33 +0300

    public instance methods that raise exceptions

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,15 +6,16 @@ that calculates base gemotry of an object.
 
 
 class BaseGeometry:
-    """Class with methods that calculate the Base Geometry"""
+    """Class with methods that calculate Base Geometry"""
     
     def area(self):
         """Public instance method raises a specific exception"""
         raise Exception("area() is not implemented")
     
     def integer_validator(self, name, value):
-        """Public instance method validates type and value of a variable"""
-        if type(value) != int:
+        """Public instance method validates type and value of variable"""
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
