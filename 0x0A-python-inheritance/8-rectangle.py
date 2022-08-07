9c806b31421a0d9c8626525a50993336b75990ff
Brook Endale <bekelebrook24@gmail.com>
2022-08-07 12:40:04 +0300

    Class inherits from class BaseGeometry and initializes

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """
-Module contains a Class that inhertis from,
-Class BaseGeometry
+This module contains a Class BaseGeometry,
+that calculates base gemotry of an object.
 """
 
 
-from 7-base_geometry import BaseGeometry
+class BaseGeometry:
+    """Class with methods that calculate Base Geometry"""
+
+    def area(self):
+        """Public instance method raises a specific exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method validates type and value of variable"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
