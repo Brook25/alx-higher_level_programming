dd66d155b28a38604d4ceee5c2e88919f47b578a
Brook Endale <bekelebrook24@gmail.com>
2022-08-08 13:33:01 +0300

    edit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,19 +5,7 @@ that calculates base gemotry of an object.
 """
 
 
-class BaseGeometry:
-    """Class with methods that calculate Base Geometry"""
-
-    def area(self):
-        """Public instance method raises a specific exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Public instance method validates type and value of variable"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
