86dd467a3d8799a0093e5530702d5db3410c2fa2
Brook Endale <bekelebrook24@gmail.com>
2022-08-08 13:40:25 +0300

    edit

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,15 +6,15 @@ that calculates base gemotry of an object.
 
 
 class BaseGeometry:
-    """Class with methods that calculate Base Geometry"""
+    """Class has public instance methods"""
     
     def area(self):
-        """Public instance method raises an exception"""
+        """Raises an exception"""
         
         raise Exception("area() is not implemented")
     
     def integer_validator(self, name, value):
-        """Public instance method validates type, value"""
+        """Method validates type, value"""
         
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
