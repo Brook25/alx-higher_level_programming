4cb8049be01d88304c97024cc5a392abe5a38a15
Brook Endale <bekelebrook24@gmail.com>
2022-08-21 21:38:00 +0300

    editing square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
-Contains the definition of the class square
+"""Contains the definition,
+of the class square.
 """
 
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 
