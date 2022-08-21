828f7b855452081dec8ba2037dc1bd3ad1d05f55
Brook Endale <bekelebrook24@gmail.com>
2022-08-21 20:51:42 +0300

    pycodestyle fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,17 +7,16 @@ BaseGeometry.
 
 class BaseGeometry:
     """Class with public instance methods."""
-    
+
     def area(self):
         """Raises an exception."""
-        
+
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Method validates type, value."""
-        
+
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
