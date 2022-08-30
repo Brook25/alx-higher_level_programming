d2c4a019e56e7e102a7ba8719d723d972b871e50
Brook25 <bekelebrook24@gmail.com>
2022-08-30 13:22:41 +0000

    documentation fix, may be a bug

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 """
-This Module.
-contains class Square class.
+This Module contains Class square.
 """
 
-
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('9-rectangle.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Class square that inherits from rectangle"""
-    
+    """Class square that inherits from rectanlge"""
+
     def __init__(self, size):
         """Class initialization function"""
-        
+
         self.integer_validator("size", size)
         super().__init__(size, size)
 
     def area(self):
         """Returns Area of the square"""
-        
-        return super().area()
 
+        return super().area()
+    
     def __str__(self):
-        """Non-formal string representation of the object"""
-        
-        return super().__str__()
+        """Non-formal string repr of the object"""
+
+        return super.__str__()
