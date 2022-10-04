e6f2dd16f1b7fffc0128ca4aa98a4eda144716c1
Brook25 <bekelebrook24@gmail.com>
2022-10-04 18:56:01 +0000

    Edits

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,10 @@
 const dict = require('./101-data').dict;
 const n_wDict = {};
 for (const key in dict) {
-  if (n_wDict[dict[key]]) {
-    n_wDict[dict[key]].push(key);
-  } else {
-    n_wDict[dict[key]] = [key];
-  }
+	if (n_wDict[dict[key]]) {
+	n_wDict[dict[key]].push(key);
+	} else {
+	n_wDict[dict[key]] = [key];
+	}
 }
 console.log(n_wDict);
