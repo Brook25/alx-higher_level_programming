47790887d25b92e8e2ffd7c1e863bd8a62a20022
Brook25 <bekelebrook24@gmail.com>
2022-10-06 09:05:45 +0000

    Edit

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
--- displays the average temperature  in Fahrenheit by city ordered by temperature (descending)
--- display an average value
-SELECT `city`, AVG(`value`) 'avg_temp' FROM temperatures GROUP BY `city` ORDER BY `avg_temp` DESC;
+-- displays the average temperature  in Fahrenheit by city ordered by temperature in descending order
+-- display average value of every city
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
