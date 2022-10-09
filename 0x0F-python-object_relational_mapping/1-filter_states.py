b8f00335390c942ffb0334ff7e21c03c5e7d29e1
Brook Endale <bekelebrook24@gmail.com>
2022-10-09 21:12:46 +0300

    Edits

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# This script that lists all states with a name starting with N
-# from the database hbtn_0e_0_usa
+"""This script that lists all states with a name starting with N"""
+
+
 import MySQLdb
 from sys import argv
 
