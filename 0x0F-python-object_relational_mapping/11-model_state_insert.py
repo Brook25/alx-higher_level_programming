5c9389e442b23c1a29317cc8699048b60124d023
Brook25 <bekelebrook24@gmail.com>
2022-10-24 08:45:29 +0000

    Edits

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,12 +8,10 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+                                   sys.argv[3]))
 
     session = Session(engine)
     session.add(State(name="Louisiana"))
