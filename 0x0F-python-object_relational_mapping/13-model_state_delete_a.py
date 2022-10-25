3f48874ddabafac21e333a85e984041c8d406886
Brook25 <bekelebrook24@gmail.com>
2022-10-25 06:43:27 +0000

    Edits

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -14,7 +14,10 @@ if __name__ == "__main__":
                                    sys.argv[3]), pool_pre_ping=True)
 
     session = Session(engine)
+    """Here you can also loop over and delete
+     returned objects using session.delete(obj)
+    """
     session.query(State).filter(State.name.like('%a%'))\
-        .delete()
+        .delete(synchronize_session=False)
     session.commit()
     session.close()
