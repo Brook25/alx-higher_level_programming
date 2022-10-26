02700a89bd4840930d6ab76ae30a648fbbab046a
Brook25 <bekelebrook24@gmail.com>
2022-10-26 10:18:32 +0000

    Edits

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Definition of the class City"""
 
+from model_state import Base, State
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import relationship
 
-Base = declarative_base()
 
 
-class City(Base):
+class City(State):
     """Class City"""
 
     __tablename__ = 'cities'
@@ -16,3 +16,6 @@ class City(Base):
                 primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://brook:Volcano27!@localhost/hbtn_0d_usa')
+    Base.metadata.create_all(engine)
