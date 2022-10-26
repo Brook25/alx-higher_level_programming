4878b98a6dd258a156b7ba1852697e2693ccda90
Brook25 <bekelebrook24@gmail.com>
2022-10-26 08:25:24 +0000

    Edits

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Script deletes all State objects with a name containing
-the letter a from the database hbtn_0e_6_usa"""
+"""Script prints all City objects
+from the database hbtn_0e_14_usa"""
 
 if __name__ == "__main__":
 
+    import sys
     from model_state import Base, State
     from model_city import City
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
-    import sys
+    from sqlalchemy.schema import Table
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
-                    .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
     session = Session(engine)
-    stmt = session.query(State.id, State.name.\
-            label('sname')).subquery()
-    for s, cid, c in session.query(stmt.c.sname, City.id, City.name)\
-            .join(stmt, City.state_id==stmt.c.id).order_by(City.id):
-        print('{}: ({}) {}'.format(s, cid, c))
+    for state, city in session.query(State, City)\
+                              .filter(City.state_id == State.id)\
+                              .order_by(City.id).all():
+            print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
