ed4e5e8e938e7ecf9a9cd839d3d49011d0569272
Brook25 <bekelebrook24@gmail.com>
2022-10-26 08:10:23 +0000

    Edits

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Script deletes all State objects with a name containing
 the letter a from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
-    
+
     from model_state import Base, State
     from model_city import City
     from sqlalchemy import create_engine
@@ -11,12 +11,11 @@ if __name__ == "__main__":
     import sys
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
+                    .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     session = Session(engine)
     stmt = session.query(State.id, State.name.\
-        label('sname')).subquery()
+            label('sname')).subquery()
     for s, cid, c in session.query(stmt.c.sname, City.id, City.name)\
-        .join(stmt, City.state_id==stmt.c.id):
+            .join(stmt, City.state_id==stmt.c.id):
         print('{}: ({}) {}'.format(s, cid, c))
     session.close()
