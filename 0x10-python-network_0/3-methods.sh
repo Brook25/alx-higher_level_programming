a3f03566ffdc838e6b61452273beb78411363c28
Brook25 <bekelebrook24@gmail.com>
2022-11-15 10:31:48 +0000

    Edits

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL and displays all HTTP methods the server will accept
-curl -sI OPTIONS "$1" | grep "Allow " | cut -d' ' -f 2-
+curl -sI -X OPTIONS "$1" | grep "Allow " | cut -d' ' -f 2-
