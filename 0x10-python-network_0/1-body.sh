642f8d8e1fa17e000cf1c69146248477214671a7
Brook25 <bekelebrook24@gmail.com>
2022-11-15 09:53:42 +0000

    Edits

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This script checks status code and prints response body
-var=$(curl -sI "$1" | head -1 | cut -d ' ' -f 2)                                             
-if [ "$var" -eq 200 ]; then                                                                  
-        echo $(curl -s "$1")                                                                 
-fi
+curl -s -X GET -L
