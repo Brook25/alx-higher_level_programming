99964e732c57120e709af71d55e668ff18bc6390
Brook25 <bekelebrook24@gmail.com>
2022-11-15 19:45:13 +0000

    Advacned tasks

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
+
+const id = process.argv[2];
+const url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    for (const i in body.characters) {
+      request(body.characters[i], function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
           console.log(JSON.parse(body).name);
         }
       });
-    });
+    }
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
   }
 });
