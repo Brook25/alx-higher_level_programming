6b99309e1af3664b4bd8c0a01e450a8c8372641c
Brook25 <bekelebrook24@gmail.com>
2022-11-15 19:39:55 +0000

    Advacned tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,17 @@
 #!/usr/bin/node
-const request = require('request');
-let url = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+/*  script prints the title of a Star Wars movie where the episode number
+matches a given integer.
+The first argument is the movie ID
+https://swapi-api.hbtn.io/api/films/:id */
+
+const arg = process.argv;
+const movieId = arg[2];
+const requestURL = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+const req = require('request');
+req(requestURL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
