789787417ae4386cdac26d1982339c1c60aa5dcf
Brook25 <bekelebrook24@gmail.com>
2022-11-18 16:53:58 +0000

    Edits

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,9 +6,8 @@ if __name__ == "__main__":
     import urllib.parse
     import sys
 
-    value = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(value)
+    data = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(data)
     data = data.encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode("utf-8", "replace"))
+    with urllib.request.urlopen(sys.argv[1], data=data) as response:
+        print(response.read().decode("utf-8"))
