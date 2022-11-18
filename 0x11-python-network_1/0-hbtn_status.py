e36d1a9e76d143063ad7c6c9726922008075ed23
Brook25 <bekelebrook24@gmail.com>
2022-11-18 15:05:30 +0000

    Edits

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
             as resp:
         content = resp.read()
         print('Body response:')
-        print('    - type: {}'.format(type(content)))
-        print('    - content: {}'.format(content))
-        print('    - utf8 content: {}'.format(content.decode("utf-8")))
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
+        print('\t- utf8 content: {}'.format(content.decode("utf-8")))
