926945d8610e8db755e2abc87beeb4773219aa2d
Brook25 <bekelebrook24@gmail.com>
2023-01-06 13:59:18 +0000

    Edits

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,7 +11,7 @@
 
 int is_palindrome(listint_t **head)
 {
-size_t num = print_listint(*head);
+size_t num = print_listint((const listint_t *) *head);
 int num_arr[num / 2];
 size_t i;
 for (i = 0; i < num / 2; i++)
@@ -34,4 +34,39 @@ i--;
 }
 
 return (1);
+}
+
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 1);
+    print_listint(head);
+
+    if (is_palindrome(&head) == 1)
+        printf("Linked list is a palindrome\n");
+    else
+        printf("Linked list is not a palindrome\n");
+
+    free_listint(head);
+
+    return (0);
 }
