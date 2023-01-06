5a33b885eb96076505d37fada508b6f41f4e7a78
Brook25 <bekelebrook24@gmail.com>
2023-01-06 16:19:56 +0000

    Edits

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,6 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+size_t count_listint(const listint_t *h)
+{
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
+
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+	//printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
+
+    return (n);
+}
+
+
+
+
 /**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: pointer to the head node
@@ -9,9 +31,10 @@
  */
 
 
+
 int is_palindrome(listint_t **head)
 {
-size_t num = print_listint((const listint_t *) *head);
+size_t num = count_listint((const listint_t *) *head);
 int num_arr[num / 2];
 size_t i;
 for (i = 0; i < num / 2; i++)
